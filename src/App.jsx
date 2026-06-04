1e98ffcb1764e3969bc1f01e4f9575ab501140f3
JhonC <jairovalencia100k19@gmail.com>
2026-06-04 11:44:30 -0500

    Vercel Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import Header from './components/Header'
 import Intro from "./components/Intro";
 import GridBackground from "./components/GridBackground";
+import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
-  
+
   return (
     <div className="min-h-screen bg-mainBlack text-white">
-    <div 
-      className="fixed left-0 top-0 bottom-0 w-[2px] z-30 pointer-events-none"
-      style={{
-        background: 'linear-gradient(to bottom, transparent, #8B1A1A 30%, #8B1A1A 70%, transparent)'
-      }}
-    />
-    <GridBackground />
-    <Header />
-    <Intro />
-    
+      <div
+        className="fixed left-0 top-0 bottom-0 w-[2px] z-30 pointer-events-none"
+        style={{
+          background: 'linear-gradient(to bottom, transparent, #8B1A1A 30%, #8B1A1A 70%, transparent)'
+        }}
+      />
+      <Analytics />
+      <GridBackground />
+      <Header />
+      <Intro />
+
 
 
     </div>
